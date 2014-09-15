e348ee2f02014fdb3dfafa16366dfd9d3826e6fc
Gansperger Istv?n <gansperger@gmail.com>
2014-09-15 10:42:25 +0200

    Do a clean build every time

--- build.cmd
+++ build.cmd
@@ -8,4 +8,5 @@ set PATH=%PATH%;%ProgramFiles%\Microsoft SDKs\F#\3.1\Framework\v4.0
 set PATH=%PATH%;%ProgramFiles%\Microsoft SDKs\F#\3.0\Framework\v4.0
 set PATH=%PATH%;tools\NuGet
 nuget install IntelliFactory.Build -nocache -pre -ExcludeVersion -o tools\packages
+fsi.exe --exec build.fsx --clean
 fsi.exe --exec build.fsx %*

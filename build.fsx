58de7a58e2679442b35ef11f6a56b0ff303b31d9
Loic Denuziere <loic.denuziere@intellifactory.com>
2015-05-15 14:25:35 +0200

    Get version from WebSharper

--- build.fsx
+++ build.fsx
@@ -2,8 +2,9 @@
 open IntelliFactory.Build
 
 let bt =
-    (BuildTool().PackageId("WebSharper.PouchDB", "3.0")
-    |> fun bt -> bt.WithFramework(bt.Framework.Net40))
+    BuildTool().PackageId("WebSharper.PouchDB")
+        .VersionFrom("WebSharper")
+        .WithFramework(fun fw -> fw.Net40)
 
 
 let main =
